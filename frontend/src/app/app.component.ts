c7105955ed7f3544c58aef54193421814a920f20
Alexey Udodov <weberdster@gmail.com>
2022-12-10 23:50:49 +0400

    added tailwind and form with validation

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
+import {FormBuilder, FormControl, Validators} from "@angular/forms";
+import {DateValidator} from "../validators/date.validator";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  currentDate = new Date();
+  maxEndDate = moment();
+
+  historyRequestForm = this.formBuilder.group({
+    ticker: ['', Validators.required],
+    startDate: [moment().subtract(7, 'days'), [Validators.required, DateValidator.dateValidator]],
+    endDate: [moment(), [Validators.required, DateValidator.dateValidator]],
+    email: ['', Validators.required, Validators.email],
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  get f() { return this.historyRequestForm.controls; }
+
+  touchedAndInvalid(): boolean {
+    return this.historyRequestForm.invalid && (this.historyRequestForm.dirty || this.historyRequestForm.touched)
+  }
+
+  onSubmit(): void {
+    this.historyRequestForm.markAllAsTouched();
+
+    if (this.historyRequestForm.invalid) {
+      return
+    }
+  }
+
+  dateValidator(control: FormControl): { [p: string]: boolean } | null {
+    if (control.value) {
+      const date = moment(control.value);
+      const today = moment();
+
+      if (date.isBefore(today)) {
+        return { 'invalidDate': true }
+      }
+    }
+    return null;
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NbThemeModule, NbLayoutModule, NbDatepickerModule} from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import {NbMomentDateModule} from "@nebular/moment";
 
 @NgModule({
   declarations: [
@@ -10,7 +15,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({name: 'default'}),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbDatepickerModule.forRoot(),
+    NbMomentDateModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
